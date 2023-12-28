4f41faf69d2f35ade6c381b8d665d6649aeb63f0
Maxim Yaroshchuk <maxim.yaroshuk@splynx.com>
2023-12-28 02:47:29 +0200

    Add valie docs site

---
+++ docs/app.config.ts
@@ -0,0 +1,38 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Light Modal',
+    description: 'Light Modal Vue 3 documentation site.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'rainoldweb',
+      github: 'maximyaroshchuk/light-modal-vue3',
+      npm: {
+        label: 'NPM',
+        icon: 'simple-icons:npm',
+        href: 'https://www.npmjs.com/package/light-modal-vue3'
+      }
+    },
+    github: {
+      dir: '.starters/default/content',
+      branch: 'main',
+      repo: 'docus',
+      owner: 'nuxt-themes',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: false,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
